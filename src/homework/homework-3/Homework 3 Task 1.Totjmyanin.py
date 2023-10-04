12105b639888859963791132a43703918489a879
iraedeus <dtotjmyanin@mail.ru>
2023-10-04 18:23:54 +0300

    docs: Added documentation to functions

--- src/homework/homework-3/Homework 3 Task 1.Totjmyanin.py
+++ src/homework/homework-3/Homework 3 Task 1.Totjmyanin.py
@@ -2,6 +2,12 @@ user_function = None
 
 
 def curry_explicit(function, arity):
+    """
+    Turning a function from several parameters into a function from one parameter that returns a function from the other parameters:
+    function(a,b) --> function(a)(b)
+    """
+    if arity == 0:
+        return function()
     global user_function
     user_function = function
 
@@ -11,21 +17,21 @@ def curry_explicit(function, arity):
 
         def curry(x):
             return get_args([*args, x])
-
         return curry
-
     return get_args([])
 
 
 def uncurry_explicit(function, arity):
+    """
+    Performs the reverse actions of the curry_explicit function:
+    function(a)(b) --> function(a,b)
+    """
     def get_args(args):
         if len(args) == arity:
             return user_function(*args)
 
         def uncurry(*x):
-            arguments = []
-            for i in range(len(x)):
-                arguments.append(x[i])
+            arguments = [item for item in x]
             return get_args(arguments)
 
         return uncurry
@@ -33,11 +39,9 @@ def uncurry_explicit(function, arity):
     return get_args([])
 
 
-input_user_function = eval(input("Enter your python function: "))
-input_user_arity = int(input("Enter your arity: "))
+if __name__ == "__main__":
+    input_user_function = eval(input("Enter your python function: "))
+    input_user_arity = int(input("Enter your arity: "))
 
-f = curry_explicit(print, 10)
-
-
-curried_function = curry_explicit(input_user_function, input_user_arity)
-print("Successful!")
+    curried_function = curry_explicit(input_user_function, input_user_arity)
+    print("Successful!")
