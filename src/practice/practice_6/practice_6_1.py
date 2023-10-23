6fd89def1b7f4ef8a1077bbbddc2ab4b94407e2b
iraedeus <dtotjmyanin@mail.ru>
2023-10-23 20:44:56 +0300

    fix: Added exception for not 3 coefficients, output now is tuples

--- src/practice/practice_6/practice_6_1.py
+++ src/practice/practice_6/practice_6_1.py
@@ -1,10 +1,10 @@
 def solve_quadratic_equation(a, b, c):
     discriminant = (b * b) - (4 * a * c)
     if discriminant == 0:
-        return -b / (2 * a)
+        return (-b / (2 * a),)
     elif discriminant > 0:
-        x1 = (-b - (discriminant**0.5)) / (2 * a)
-        x2 = (-b + (discriminant**0.5)) / (2 * a)
+        x1 = (-b - (discriminant ** 0.5)) / (2 * a)
+        x2 = (-b + (discriminant ** 0.5)) / (2 * a)
         return x1, x2
     else:
         raise ValueError("Discriminant must be greater or equal than 0")
@@ -12,14 +12,14 @@ def solve_quadratic_equation(a, b, c):
 
 def solve_linear_equation(b, c):
     if b != 0:
-        return -c / b
+        return (-c / b,)
     else:
         raise ZeroDivisionError
 
 
 def solve_equation(a, b, c):
     if (a == 0) and (b == 0) and (c == 0):
-        return
+        return ()
     elif a == 0:
         return solve_linear_equation(b, c)
     return solve_quadratic_equation(a, b, c)
@@ -34,21 +34,21 @@ def is_float_number(number):
 
 
 def to_float_coeffs(user_input):
-    for i in user_input:
-        if not is_float_number(i):
-            raise ValueError("You entered wrong coefficients")
-        return map(float, user_input)
+    user_input = user_input.split(" ")
+    if len(user_input) == 3:
+        for i in user_input:
+            if not is_float_number(i):
+                raise ValueError(
+                    "You entered wrong coefficients: They must be float number"
+                )
+            return list(map(float, user_input))
+    else:
+        raise ValueError("You must enter 3 coefficients")
 
 
 def main():
-    coeffs = input("Enter your coefficients: ").split(" ")
-    a, b, c = to_float_coeffs(coeffs)
-    roots = solve_equation(a, b, c)
-
-    if type(roots) == float:
-        print(roots)
-    else:
-        print(roots[0], roots[1])
+    a, b, c = to_float_coeffs(input("Enter your coefficients: "))
+    print(solve_equation(a, b, c))
 
 
 if __name__ == "__main__":
--- tests/practice/practice-6/test_practice_6_1.py
+++ tests/practice/practice-6/test_practice_6_1.py
@@ -2,37 +2,55 @@ import pytest
 from io import StringIO
 from src.practice.practice_6.practice_6_1 import *
 
-is_float_arr = [("9.9", True), ("9", True), ("a", False), ("*", False)]
-
-solve_linear_arr = [
-    (6, 1, -1 / 6),
-    (6, 0, 0),
+to_float_arr = [
+    ("5 8 9", [5.0, 8.0, 9.0]),
+    ("0 66 9", [0.0, 66.0, 9.0]),
+    ("9.83 0 9.8", [9.83, 0.0, 9.8]),
 ]
 
-linear_exceptions_arr = [(0, 6), (0, 700)]
+solve_linear_arr = [
+    (6, 1, (-1 / 6,)),
+    (6, 0, (0,)),
+]
 
-solve_quadratic_arr = [(3, 4, 1, [-2 / 6, -1]), [2, -14, 24, [3, 4]], [1, -4, 4, 2]]
-
-quadratic_exceptions_arr = [(1, 6, 999), (9, 83, 7373773)]
+solve_quadratic_arr = [
+    (3, 4, 1, (-2 / 6, -1.0)),
+    (2, -14, 24, (3.0, 4.0)),
+    (1, -4, 4, (2.0,)),
+]
 
 solve_equation_arr = [
-    (1, -6, 9, 3),
-    (1, 6, 9, -3),
-    (0, 5, 1, -1 / 5),
-    (5, -8, 3, [1, 3 / 5]),
+    (1, -6, 9, (3,)),
+    (1, 6, 9, (-3,)),
+    (0, 5, 1, (-1 / 5,)),
+    (5, -8, 3, (1, 3 / 5)),
 ]
 
 main_scenario_arr = [
-    ("1 17 -18", [1.0, -18.0]),
-    ("2 7 -4", [1 / 2, -4.0]),
-    ("1 18 81", -9.0),
-    ("0 11 5", -5 / 11),
+    ("1 17 -18", "(1.0, -18.0)\n", "(-18.0, 1.0)\n"),
+    ("2 7 -4", "(0.5, -4.0)\n", "(-4.0, 0.5)\n"),
+    ("1 18 81", "(-9.0,)\n", ""),
+    ("0 11 5", f"({str(-5/11)},)\n", ""),
+    ("0 0 0", "()\n", ""),
 ]
 
 
-@pytest.mark.parametrize("number,expected", is_float_arr)
+@pytest.mark.parametrize(
+    "number,expected", [("9.9", True), ("9", True), ("a", False), ("*", False)]
+)
 def test_is_float(number, expected):
     assert is_float_number(number) == expected
+
+
+@pytest.mark.parametrize("numbers,expected", to_float_arr)
+def test_to_float(numbers, expected):
+    assert to_float_coeffs(numbers) == expected
+
+
+@pytest.mark.parametrize("numbers", [("88 *^& 8"), ("jj k l")])
+def test_to_float_exceptions(numbers):
+    with pytest.raises(ValueError):
+        to_float_coeffs(numbers)
 
 
 @pytest.mark.parametrize("b,c,expected", solve_linear_arr)
@@ -40,7 +58,7 @@ def test_solve_linear_equation(b, c, expected):
     assert solve_linear_equation(b, c) == expected
 
 
-@pytest.mark.parametrize("b,c", linear_exceptions_arr)
+@pytest.mark.parametrize("b,c", [(0, 6), (0, 700)])
 def test_linear_exceptions(b, c):
     with pytest.raises(ZeroDivisionError):
         solve_linear_equation(b, c)
@@ -49,13 +67,10 @@ def test_linear_exceptions(b, c):
 @pytest.mark.parametrize("a,b,c,expected", solve_quadratic_arr)
 def test_solve_quadratic_equation(a, b, c, expected):
     actual = solve_quadratic_equation(a, b, c)
-    if type(actual) == float:
-        assert actual == expected
-    else:
-        assert actual[0] in expected and actual[1] in expected
+    assert set(actual) == set(expected)
 
 
-@pytest.mark.parametrize("a,b,c", quadratic_exceptions_arr)
+@pytest.mark.parametrize("a,b,c", [(1, 6, 999), (9, 83, 7373773)])
 def test_quadratic_exceptions(a, b, c):
     with pytest.raises(ValueError):
         solve_quadratic_equation(a, b, c)
@@ -67,22 +82,14 @@ def test_solve_equations(a, b, c, expected):
         assert solve_equation(a, b, c) == expected
     else:
         actual = solve_equation(a, b, c)
-        if type(actual) == float:
-            assert actual == expected
-        else:
-            assert actual[0] in expected and actual[1] in expected
+        assert set(actual) == set(expected)
 
 
-@pytest.mark.parametrize("input_user, expected", main_scenario_arr)
-def test_main_scenario(input_user, expected, monkeypatch):
+@pytest.mark.parametrize("input_user, expected1, expected2", main_scenario_arr)
+def test_main_scenario(input_user, expected1, expected2, monkeypatch):
     monkeypatch.setattr("builtins.input", lambda _: input_user)
     fake_output = StringIO()
     monkeypatch.setattr("sys.stdout", fake_output)
     main()
     output = fake_output.getvalue()
-    output = output.rstrip("\n").split(" ")
-
-    if len(output) == 1:
-        assert float(output[0]) == expected
-    else:
-        assert float(output[0]) in expected and float(output[1]) in expected
+    assert (output == expected1) or (output == expected2)
