609f40965592d552bbd4df54b4a856ca3a94f73b
iraedeus <dtotjmyanin@mail.ru>
2023-11-06 20:00:17 +0300

    Initial commit

---
+++ src/homework/homework_8/homework_8_1.py
@@ -0,0 +1,50 @@
+def to_unicode_char(char):
+    hex_unicode = hex(ord(char)).lstrip("0x").upper()
+    return "U+" + hex_unicode.rjust(4, "0")
+
+
+def to_utf16(char):
+    output = ""
+    decimal_char = ord(char)
+
+    if decimal_char < 65535:
+        binary_char = bin(decimal_char).lstrip("0b").rjust(16, "0")
+        output = binary_char[:8] + " " + binary_char[8:]
+    else:
+        decimal_char -= 65536
+        binary_char = bin(decimal_char).lstrip("0b").rjust(20, "0")
+
+        higher_num = int(binary_char[:10], 2) + 55296
+        lower_num = int(binary_char[10:], 2) + 56320
+
+        higher_bits = bin(higher_num).lstrip("0b")
+        lower_bits = bin(lower_num).lstrip("0b")
+
+        output = (
+            higher_bits[:8]
+            + " "
+            + higher_bits[8:]
+            + " "
+            + lower_bits[:8]
+            + " "
+            + lower_bits[8:]
+        )
+
+    return output
+
+
+def main():
+    string = input("Enter your string: ")
+    output = ""
+    for i in string:
+        char = i
+        unicode_char = to_unicode_char(char)
+        utf16 = to_utf16(char)
+
+        output += f"{char}    {unicode_char}    {utf16}\n"
+
+    return output
+
+
+if __name__ == "__main__":
+    print(main())
