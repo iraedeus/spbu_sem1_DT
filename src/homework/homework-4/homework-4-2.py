198ec9633763363b07a8d5d128cecf1e9a71ca93
iraedeus <dtotjmyanin@mail.ru>
2023-11-17 15:53:55 +0300

    fix: Fixed a problem with zero, the program incorrectly gave an answer

--- src/homework/homework-4/homework-4-2.py
+++ src/homework/homework-4/homework-4-2.py
@@ -1,4 +1,4 @@
-from math import *
+from math import floor
 
 
 def to_bin_fractional(number, mantissa_bits):
@@ -23,7 +23,7 @@ def find_mantissa(number, k, bits):
     integer_part = floor(abs(number))
     fractional_part = abs(number) - integer_part
 
-    binary_integer_part = bin(integer_part).lstrip("0b")
+    binary_integer_part = bin(integer_part)[2:]
     mantissa_bits = bits - len(binary_integer_part) - k
     binary_fractional_part = to_bin_fractional(fractional_part, mantissa_bits)
 
@@ -32,37 +32,46 @@ def find_mantissa(number, k, bits):
 
 def find_true_order(number):
     integer_part = floor(abs(number))
-    binary_integer_part = bin(integer_part).lstrip("0b")
+    binary_integer_part = bin(integer_part)[2:]
 
     return len(binary_integer_part) - 1
 
 
 def to_fp16(number):
-    true_order = find_true_order(number)
-    shifted_order = true_order + 2 ** 4 - 1
+    if number != 0:
+        true_order = find_true_order(number)
+        shifted_order = true_order + 2**4 - 1
 
-    mantissa = find_mantissa(number, 5, 16)
-    return check_sign(number) + bin(shifted_order).lstrip("0b") + " " + mantissa
+        mantissa = find_mantissa(number, 5, 16)
+        return check_sign(number) + bin(shifted_order).lstrip("0b") + " " + mantissa
+    else:
+        return "0 " + "0" * 5 + " " + "0" * 11
 
 
 def to_fp32(number):
-    true_order = find_true_order(number)
-    shifted_order = true_order + 2 ** 7 - 1
+    if number != 0:
+        true_order = find_true_order(number)
+        shifted_order = true_order + 2**7 - 1
 
-    mantissa = find_mantissa(number, 8, 32)
-    return check_sign(number) + bin(shifted_order).lstrip("0b") + " " + mantissa
+        mantissa = find_mantissa(number, 8, 32)
+        return check_sign(number) + bin(shifted_order).lstrip("0b") + " " + mantissa
+    else:
+        return "0 " + "0" * 8 + " " + "0" * 23
 
 
 def to_fp64(number):
-    true_order = find_true_order(number)
-    shifted_order = true_order + 2 ** 10 - 1
+    if number != 0:
+        true_order = find_true_order(number)
+        shifted_order = true_order + 2**10 - 1
 
-    mantissa = find_mantissa(number, 11, 64)
-    return check_sign(number) + bin(shifted_order).lstrip("0b") + " " + mantissa
+        mantissa = find_mantissa(number, 11, 64)
+        return check_sign(number) + bin(shifted_order).lstrip("0b") + " " + mantissa
+    else:
+        return "0 " + "0" * 11 + " " + "0" * 52
 
 
 def to_exponential(number):
-    if number > 0:
+    if number >= 0:
         output = "+"
     else:
         output = "-"
@@ -76,16 +85,25 @@ def to_exponential(number):
 
 
 def output_to_user(choice, number):
-    print(f"Your number in exponential format: {to_exponential(number)}")
+    exponential_number = to_exponential(number)
     if choice == "1":
+        print(f"Your number in exponential format: {exponential_number}")
         print(f"Your number in FP16: {to_fp16(number)}")
     if choice == "2":
+        print(f"Your number in exponential format: {exponential_number}")
         print(f"Your number in FP32: {to_fp32(number)}")
     if choice == "3":
+        print(f"Your number in exponential format: {exponential_number}")
         print(f"Your number in FP64: {to_fp64(number)}")
+    else:
+        print("You have chosen an incorrect action")
 
 
 if __name__ == "__main__":
-    user_number = float(input("Enter you number: ").replace(",", "."))
-    user_choice = input("Choose format: \n" "1) FP16 \n" "2) FP32 \n" "3) FP64 \n")
-    output_to_user(user_choice, user_number)
+    try:
+        user_number = float(input("Enter you number: ").replace(",", "."))
+        user_choice = input("Choose format: \n" "1) FP16 \n" "2) FP32 \n" "3) FP64 \n")
+        output_to_user(user_choice, user_number)
+        print(bin(0).lstrip("0b"))
+    except ValueError:
+        print("You entered not a number")
