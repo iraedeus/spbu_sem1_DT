1fa992f7adb1fdca138320b4d1228618a48e2ce9
iraedeus <dtotjmyanin@mail.ru>
2023-12-20 12:37:08 +0300

    fix: Fixed main scenario test

--- tests/homework/homework_13/test_homework_13_1.py
+++ tests/homework/homework_13/test_homework_13_1.py
@@ -8,35 +8,25 @@ import builtins
 @pytest.mark.parametrize(
     "input_user, expected",
     [
-        ("abb", "Your string is word of a regular expression: (a|b)+abb"),
+        (["abb", "Exit"], "Your string is word of a regular expression: (a|b)+abb\n"),
         (
-            "ababa",
+            ["ababa", "Exit"],
             "",
         ),
         (
-            "45.45",
-            "Your string is word of a regular expression: digit+(.digit+)?(E(+|-)?digit+)?",
+            ["45.45", "Exit"],
+            "Your string is word of a regular expression: digit+(.digit+)?(E(+|-)?digit+)?\n",
         ),
         (
-            "45E",
+            ["45E", "Exit"],
             "",
         ),
     ],
 )
 def test_main_scenario(input_user, expected, monkeypatch):
-    inputs = [input_user, "Exit"]
-    prints = []
-
-    def mock_input(s):
-        return inputs.pop(0)
-
-    def mock_print(prints):
-        return lambda s: prints.append(s)
-
-    def mock_start_input_output(prints):
-        builtins.input = mock_input
-        builtins.print = mock_print(prints)
-
-    mock_start_input_output(prints)
+    monkeypatch.setattr("builtins.input", lambda _: input_user.pop(0))
+    fake_output = StringIO()
+    monkeypatch.setattr("sys.stdout", fake_output)
     main()
-    assert len(prints) == 1 or prints[1] == expected
+    output = fake_output.getvalue()
+    assert output.lstrip("If you want exit, enter 'Exit'\n") == expected
